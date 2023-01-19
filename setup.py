35056fc3be1c92f38a4a74a5bce34d0eb937a84b
Tim Cosemans <tim_cosemans@hotmail.com>
2023-01-19 09:55:53 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Marketing analytics proof of concept Smeg',
+    author='TimCosemans',
+    license='',
+)
